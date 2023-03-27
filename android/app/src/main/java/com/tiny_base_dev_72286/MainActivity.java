7f7a0946f18678b42f847faf3a4974ed246a6512
Crowdbotics <engineering@crowdbotics.com>
2023-03-27 01:51:20 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/tiny_base_dev_72286/MainActivity.java
@@ -0,0 +1,15 @@
+package com.tiny_base_dev_72286;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "tiny_base_dev_72286";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "tiny-base-dev-72286",
+  url: "https://tiny-base-dev-72286.botics.co",
+  api: "https://tiny-base-dev-72286.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
